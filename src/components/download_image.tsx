e99cb9926c94af710b47c52f44216322a251bef6
PedroHVM <pedromalheiros987@gmail.com>
2023-11-13 17:07:22 -0300

    last change

--- src/components/download_image.tsx
+++ src/components/download_image.tsx
@@ -26,7 +26,7 @@ const Download = () => {
                 Imóvel Guide
                 </p>
           </section>
-          <img src="https://cdn.uso.com.br/34392/2023/03/2f0a3004c192bb2f9dd6dce80361121d.jpg" alt="Mansão" />
+          <img src="https://lh3.googleusercontent.com/pw/ADCreHc3oroqv6WratsklCj9Fa9VL62GlG0j302HxDKQor2AeCCmpg8Ja7IBREX1KHx2S-T3JXpC3LAAXxW-W1L-KQR2GzADpAVtU3L6xThulbDdFB8C2lYoHOk8F-9c1Rr_6dkozKkgQYDNfDu_39plvl149M3Ps7Ireby6gexWMvmD2b37AX0wJf2Mt3bPGziSII2ptNjM1dwTOne--tSK1VH7XVdUF9a6KTSXvBPWyJWtOQE_4LTFmKA_KY7bE9hRZNCPR5DBa4K4fTa5PbfxuV7uIZ-5fe4ze8RT6Ua7cY5Xh6zx3uCqBHwT82JOGapV-ipFOsfqgoLIG7vjGw3OS45duEVSB29rUaFmVbSg0fKTJCQB_XBAZMfVssAlII4XWVihfKVJTGK1vt2jUeuubMzykA0-clYXlRCjvZwkPqP8cYzIiYVpKjiRA1zoHFty8ZrPAmvtWbQIu6Y-0c6X4KI3aJbnDEgQ9P4HaBEucYcyo3qz_7S66-cySAScOONnSruPnZJmPAe6kjtmHf8XLKBm_TErQ7AAYZaMA4LU7kU8CdygNZNWY08MuKE16i9jGqVGUFoGk5GD4-36U1DFF3IPStMBlOCwWmXfHDDVskTMAUWBbTpi9t3e9Vo1EFJg59T2ToS0q2sikfXNCXX6y03CLe20pL0kyGmVlWBmPwy6v6ZN4Zs_yX95Hqs5uGxzNCSCHcuUVB9J6wkZ5-cH7V-ME-zPURvyigAfyYmTTfLvA3ww5lpXSh9MZMeJWzflbbCnZAcNz5uN2ksFkT_5HU0rxAefNT_p_vDF5jw-Z8BCBamAHmHYv6_Rvj75p_ltiRFLxX1zqBMLRFAs9-GhgAAvaxhG4nfYzOFDWZmFBlJhQHDwTnkFFggzmiovYmFqua2S3hH4Aw3ZLXYADQortQ=w1000-h666-s-no?authuser=0" alt="Mansão" />
           <div className='overlay'>
             <button className='download_image' onClick={handleDownload}>
               Download
--- src/components/phone.tsx
+++ src/components/phone.tsx
@@ -43,7 +43,7 @@ const Phone = () => {
       <span>1</span>
       <div className="box">
         <div className="profile">
-          <img src="https://292aa00292a014763d1b-96a84504aed2b25fc1239be8d2b61736.ssl.cf1.rackcdn.com/PostImagem/22811/5-fatores-que-influenciam-para-o-seu-perfil-no-linkedin_abradilan-20-anos-62_capa_3.jpg" alt="Imagem de um corretor" />
+          <img src="https://lh3.googleusercontent.com/pw/ADCreHd9idD-1WA3cIFsgxEOUE3BJ1Wqv3iL3POaJqrGTC6B2AjVN4sQ8CIE5WV_9OIAcgDlvp6clH4fwVRUkyw0KTmjMq6HuLFjmt9Z09BFXhq2uCqxhkCjEHIpHbj4jlgprIV5s1hnIHqB41tq19cuXvLz2Fq0QuE6BYnH9RtZ2MjQs5603KD31lXmINkOwgkowm9IxmYfcs_bBSXwIBLGzYtrrWi10sU1s7_TWdho4FbljC8GOwwmUGY7eqV2Xd0vZTd4PvqKNXs1SDJnrI_CWmPvr8lrStpoS80aXuHrfaqOYfoL-VrqudKz74JlnDuSfn8oMKeXJ11Va-tYYCDfk8zijXC17Oxr61zhEpxyCKzXvBMsSs7nxRjS6_SgKRvBoG1cu3UElFeubYmtmbm2CkbYTsTgpZh9vLghP3y3lhKlsKHAx-W4BLdF8PR8jKoBqw7-X5SNNfBE91X5SFACDlxx25og_DsMiRxs4EEwSc-S8XvF5G1vOorxxDpxcidOx3E_kyWKoAmV2wCHGPPtfU045STLQr7EoddygCwOocW-BM9QRhkdAvjH5dH5cI8GU7eraUryVAWr5aho7ZhmSLCsMpNhFf_hQhyOTiPC-EP0y2RC-U05pxE4KQhsCUHGFi0B3gnsBoyrDXnjEKSTuiijy-597LFnErU3CWXMc0HL5NQvfLUMo9Sxs8tapUVD7mtZgfNSuGAi1gEtY3Rz_CS6JPCSqeFScVXd6DTqfdUbtgQF_y5cN0al2WrD8826tm1J8jg6eetVdWLzydG94y2q3zoFm68RZzv90dbKk1V0ay0TnMeMgVVLBrPwg_W0Q64pUzQh3nmon4anojMHtaxDReeDBpGqN9IXbkhg5PbRY4JxdcO8GwJtW_Wjtbrr0LJDMhwPbHLN2FyL7tby6g=w223-h221-s-no?authuser=0" alt="Imagem de um corretor" />
           <ul>
             <h1>Pedro Malheiros</h1>
             <li>3.000 pontos</li>
@@ -53,7 +53,7 @@ const Phone = () => {
           </ul>
         </div>
         <div className="profile">
-          <img src="https://evorastudio.com.br/wp-content/uploads/2022/04/retrato-corporativo-foto-perfil-profissional-foto-linkedin-63am-scaled.jpg" alt="Imagem de um corretor" />
+          <img src="https://lh3.googleusercontent.com/pw/ADCreHe1JH-w_atkJQRvGWb_M_XgR0KWhdPZSl3G37onQHN8z-nOQr45sA3AFFY8J3p8p6qXSl789Qsx6zyfQdc37y_z4PoAhI3uv8G4anuuncHrl3MvZv-sARJBP87fw2Gs773J2Z9P4ckCnStm-pwn1KNugkfI0nvx88IQnUL2joybIuH3nds_AR3MOAYdEjHfjoSQEamVmj2uxNp3XKdGtYjLOiKDVkAKz9se6WjLMqFEGaJ3vzTTlLWiOFNdLqn2R1K0LoiIERhkPW0Xa1YiR2iXCb6iWahotUt8Qm8R4kKf8qieihfVXKenRxtoQql4gfqIh1uJoLnPqTeduuHra74z55gVyw-JchMZpmDeBxux5qGvtyHlyBeqxg0lJ6lVT4MrTFEL1y5R3Rz043cKiD-jis8TadZzhVbnxB3SMMiUsZDHrK_yxZZjGzZ5G9iNjCE7huN1H7T37yvI2jEC4ifOoUodqreUNqm4fnja4RZUDLkAWb1TXusgBbRfgqsxKBsOINfvtLjj928AUVP5VrATox61Z4Y6YT37yEvz3N1E8u7tx5h05rqG34sQizn_eTBeLgqVnFvwslqpR_cM2PRUKtHRo_mRHZ_teZZgRzhrIyUu_1Us36OfRONU-aWueqj4U2fdR51jujtJiXFHzNpjw1AA70i4ms8vnIiSr3oDsSCK5-WzciIpBxT4dN_7lJv_XV1_OV7Snwx7jvhx2ftmTc2AYu54EtZ_ewixhee8yQeRJMcqddOJ47ubWfW9-BQcKHDuSF2SeKwoP9Fx73yfJ_KfKkjtptOdqk6hodKVbvX4ZZJ0wyvupQ2Zio7NlckpCo-N9ABAmzF68XGDLaSd-JqL5AHutffNRJb45RMmgP3dW9bZyavka2dBvLS4tM1vinkRssJRKqU-mGCDYg=w221-h223-s-no?authuser=0" alt="Imagem de um corretor" />
           <ul>
             <h1>Lorena Paiva</h1>
             <li>1.260 pontos</li>
